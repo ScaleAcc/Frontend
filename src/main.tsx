9a33fd4057e52d9e24f0ce734f2917fe20d25b49
EHAB3x <ehabmah6969@gmail.com>
2024-06-26 09:15:59 +0200

    project structure

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client'
+// Redux
+// Styles
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@styles/global.css"
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <p>Start the project</p>
+);
