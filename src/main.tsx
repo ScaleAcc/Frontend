bbe8f9f86466e935d92339e730ee77319c41cd6b
EHAB3x <ehabmah6969@gmail.com>
2024-06-26 11:15:44 +0200

    finish login page

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,6 @@ import Login from "@pages/Login"
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@styles/global.css"
 //Providers
-import { NextUIProvider } from '@nextui-org/react';
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <NextUIProvider>
         <Login />
-    </NextUIProvider>
 );
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,19 @@
 import { FloatingLabel } from '@components/forms'
-import React from 'react'
+import styles from "@styles/login.module.css"
+import Wave from "@assets/svg/wave.svg?react";
 const Login = () => {
   return (
-    <div className="login__form">
+    <div className={styles.login__form}>
         <form>
+          <h2 className={styles.login__header}>Scale</h2>
           <FloatingLabel type="email" label="Email"/>
           <FloatingLabel type="password" label="Password"/>
+
+          <input type="submit" name="submit" id="submit" />
         </form>
+        <div className={styles.wave__icon}>
+          <Wave />
+        </div>
     </div>
   )
 }
