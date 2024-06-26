7c5cef1fe7142f2e4c6a1a62c560301a2a8a38ca
EHAB3x <ehabmah6969@gmail.com>
2024-06-26 09:47:47 +0200

    install next-ui library

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,15 @@ import ReactDOM from 'react-dom/client'
 // Styles
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@styles/global.css"
-
+import { NextUIProvider } from '@nextui-org/react';
+import {Input} from "@nextui-org/react";
+const placements = [
+    "inside",
+    "outside",
+    "outside-left",
+  ];
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <p>Start the project</p>
+    <NextUIProvider>
+        <p>SS</p>
+    </NextUIProvider>
 );
