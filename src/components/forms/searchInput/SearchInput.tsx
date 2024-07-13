77146e699b6cf0d934decced09d8941fd3dd307d
EHAB3x <ehabmah6969@gmail.com>
2024-07-13 14:03:56 +0200

    create search input

--- src/components/forms/searchInput/SearchInput.tsx
+++ src/components/forms/searchInput/SearchInput.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import styles from './styles.module.css';
+import { IoIosSearch } from "react-icons/io";
 
+const {search__input} = styles;
 const SearchInput = () => {
   return (
-    <div>SearchInput</div>
+    <div className={search__input}>
+        <input type="text" name="search" id="search" />
+        <label htmlFor="search">
+            <IoIosSearch />
+        </label>
+    </div>
   )
 }
 
--- src/pages/Vault.tsx
+++ src/pages/Vault.tsx
@@ -1,6 +1,7 @@
 import Heading from "@components/common/Heading/Heading";
 import { Link } from "react-router-dom";
 import styles from "@styles/vault.module.css";
+import { SearchInput } from "@components/forms";
 
 const {vault__header} = styles
 const Vault = () => {
@@ -10,6 +11,10 @@ const Vault = () => {
         <Heading title="كل الخزنات الرئيسية"/>
 
         <Link className="btn-primary" to="/vault/add-new">اضافة جديدة</Link>
+      </div>
+
+      <div className="vault__search">
+        <SearchInput />
       </div>
     </div>
   )
