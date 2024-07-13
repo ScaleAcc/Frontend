596a084ff36af266b9e4dde9a9c783a089e3f5db
EHAB3x <ehabmah6969@gmail.com>
2024-07-13 14:07:28 +0200

    DD Table to vault page

--- src/pages/Vault.tsx
+++ src/pages/Vault.tsx
@@ -2,19 +2,51 @@ import Heading from "@components/common/Heading/Heading";
 import { Link } from "react-router-dom";
 import styles from "@styles/vault.module.css";
 import { SearchInput } from "@components/forms";
-
-const {vault__header} = styles
+import Table from 'react-bootstrap/Table';
+const {vault__page, vault__header, vault__search} = styles
 const Vault = () => {
   return (
-    <div className="page__container">
+    <div className={`page__container ${vault__page}`}>
       <div className={vault__header}>
         <Heading title="كل الخزنات الرئيسية"/>
 
         <Link className="btn-primary" to="/vault/add-new">اضافة جديدة</Link>
       </div>
 
-      <div className="vault__search">
+      <div className={vault__search}>
         <SearchInput />
+      </div>
+
+      <div className="vault__table">
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan={2}>Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
       </div>
     </div>
   )
