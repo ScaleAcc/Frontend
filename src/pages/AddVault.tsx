0c1753118f30bc8f60bff2f398fd9a7e3a6e7a3e
Nabil Alex <nabilmohaed46@gmail.com>
2024-07-25 19:59:12 +0300

    add form hook

--- src/pages/AddVault.tsx
+++ src/pages/AddVault.tsx
@@ -2,98 +2,107 @@ import { useForm, SubmitHandler } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { AddVaultSchema } from "@/src/validations/addvalut";
 import { Button } from "../components/ui/button";
-import { Label } from "@components/ui/label";
 import { Textarea } from "@components/ui/textarea";
 import { Input } from "@components/ui/input";
-
-interface Iprops {
-  vaultCode: string;
-  vaultName: string;
-  currentBalance: string;
-  description: string;
-}
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@components/ui/form";
 
 const AddVault = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<Iprops>({
+  const form = useForm<z.infer<typeof AddVaultSchema>>({
     mode: "onBlur",
     resolver: zodResolver(AddVaultSchema),
   });
-  const submitForm: SubmitHandler<Iprops> = (data) => {
+  const submitForm: SubmitHandler<z.infer<typeof AddVaultSchema>> = (data) => {
     console.log(data);
   };
   return (
-    <form className="mt-16 pt-16 p-6" onSubmit={handleSubmit(submitForm)}>
-      <h2 className="text-lg font-medium mb-4">إضافة خزنة</h2>
-      <div className="grid grid-cols-3 gap-4 mb-4">
-        <div className="col-span-1">
-          <Label htmlFor="vaultCode">كود الخزنة</Label>
-          <Input
-            type="text"
-            id="vaultCode"
-            placeholder="كود الخزنة"
-            {...register("vaultCode")}
-          />
-          {errors.vaultCode && (
-            <span className="text-red-500 text-sm mt-1">
-              {errors.vaultCode.message}
-            </span>
-          )}
+    <Form {...form}>
+      <form
+        className="mt-16 pt-16 p-6"
+        onSubmit={form.handleSubmit(submitForm)}
+      >
+        كود الخزنة اسم الخزنة
+        <h2 className="text-lg font-medium mb-4">إضافة خزنة</h2>
+        <div className="grid grid-cols-3 gap-4 mb-4">
+          <div className="col-span-1">
+            <FormField
+              control={form.control}
+              name="vaultCode"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>كود الخزنة</FormLabel>
+                  <FormControl>
+                    <Input placeholder="من فضلك ادخل كود الخزنة" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="col-span-1">
+            <FormField
+              control={form.control}
+              name="vaultName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>اسم الخزنة</FormLabel>
+                  <FormControl>
+                    <Input placeholder="من فضلك ادخل اسم الخزنة" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="col-span-1">
+            <FormField
+              control={form.control}
+              name="currentBalance"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>رصيد الخزنة الحالي</FormLabel>
+                  <FormControl>
+                    <Input placeholder="من فضلك ادخل رصيد الخزنة" {...field} />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
         </div>
-        <div className="col-span-1">
-          <Label htmlFor="vaultName">اسم الخزنة</Label>
-          <Input
-            type="text"
-            id="vaultName"
-            placeholder="اسم الخزنة"
-            {...register("vaultName")}
+        <div className="mb-4">
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>وصف الخزنة</FormLabel>
+                <FormControl>
+                  <Textarea placeholder="من فضلك ادخل وصف الخزنة" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
           />
-          {errors.vaultName && (
-            <span className="text-red-500 text-sm mt-1">
-              {errors.vaultName.message}
-            </span>
-          )}
         </div>
-        <div className="col-span-1">
-          <Label htmlFor="currentBalance">رصيد الخزنة الحالي</Label>
-          <Input
-            type="text"
-            id="currentBalance"
-            placeholder="رصيد الخزنة الحالي"
-            {...register("currentBalance")}
-          />
-          {errors.currentBalance && (
-            <span className="text-red-500 text-sm mt-1">
-              {errors.currentBalance.message}
-            </span>
-          )}
+        <div className="flex justify-end">
+          <Button className="mr-2 bg-light-blue text-white">
+            الرجوع للرئيسية
+          </Button>
+          <Button className="bg-teal-500 text-white" type="submit">
+            إضافة جديدة
+          </Button>
         </div>
-      </div>
-      <div className="mb-4">
-        <Label htmlFor="description">بيان</Label>
-        <Textarea
-          id="description"
-          placeholder="بيان"
-          {...register("description")}
-        />
-        {errors.description && (
-          <span className="text-red-500 text-sm mt-1">
-            {errors.description.message}
-          </span>
-        )}
-      </div>
-      <div className="flex justify-end">
-        <Button className="mr-2 bg-light-blue text-white">
-          الرجوع للرئيسية
-        </Button>
-        <Button className="bg-teal-500 text-white" type="submit">
-          إضافة جديدة
-        </Button>
-      </div>
-    </form>
+      </form>
+    </Form>
   );
 };
 export default AddVault;
