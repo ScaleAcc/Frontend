217308fb1a2417195119594a5de37ece3d2bbecb
Nabil Alex <nabilmohaed46@gmail.com>
2024-07-26 18:02:58 +0300

    .

--- src/pages/AccountsTree.tsx
+++ src/pages/AccountsTree.tsx
@@ -1,9 +1,8 @@
-import Heading from '@components/common/Heading/Heading'
+import Heading from "@components/common/Heading/Heading";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { AddVaultSchema } from "@/src/validations/addvalut";
 import { Button } from "../components/ui/button";
-import { Textarea } from "@components/ui/textarea";
 import { Input } from "@components/ui/input";
 import { z } from "zod";
 import {
@@ -14,62 +13,101 @@ import {
   FormLabel,
   FormMessage,
 } from "@components/ui/form";
+
 const AccountsTree = () => {
-    const form = useForm<z.infer<typeof AddVaultSchema>>({
-        mode: "onBlur",
-        resolver: zodResolver(AddVaultSchema),
-    });
-        const submitForm: SubmitHandler<z.infer<typeof AddVaultSchema>> = (data) => {
-        console.log(data);
-    };
+  const form = useForm<z.infer<typeof AddVaultSchema>>({
+    mode: "onBlur",
+    resolver: zodResolver(AddVaultSchema),
+  });
+
+  const submitForm: SubmitHandler<z.infer<typeof AddVaultSchema>> = (data) => {
+    console.log(data);
+  };
+
   return (
-    <div className="page__container">
-        <Heading title="شجرة الحسابات" />
+    <div className="page__container p-4 md:p-8">
+      <Heading title="شجرة الحسابات" />
 
-        <Form {...form}>
-            <form
-            className="pt-8 p-6"
-            onSubmit={form.handleSubmit(submitForm)}
-            >
-                <div className="grid grid-cols-2 gap-4 mb-4">
-                    <div className="col-span-1">
-                        <FormField
-                        control={form.control}
-                        name="vaultCode"
-                        render={({ field }) => (
-                            <FormItem>
-                            <FormLabel>كود الخزنة</FormLabel>
-                            <FormControl>
-                                <Input placeholder="اختر اسم و نوع الحساب" {...field} />
-                            </FormControl>
-                            <FormMessage />
-                            </FormItem>
-                        )}
+      <Form {...form}>
+        <form className="pt-8" onSubmit={form.handleSubmit(submitForm)}>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+            <div className="col-span-1">
+              <FormField
+                control={form.control}
+                name="vaultCode"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>كود الخزنة</FormLabel>
+                    <FormControl>
+                      <Input placeholder="من فضلك ادخل كود الخزنة" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="col-span-1">
+              <FormField
+                control={form.control}
+                name="vaultName"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>اسم الخزنة</FormLabel>
+                    <FormControl>
+                      <Input placeholder="من فضلك ادخل اسم الخزنة" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          </div>
+          <div className="mb-4">
+            <FormField
+              control={form.control}
+              name="currentBalance"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>رصيد الخزنة الحالي</FormLabel>
+                  <FormControl>
+                    <Input placeholder="من فضلك ادخل رصيد الخزنة" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="mb-4">
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>وصف الخزنة</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="من فضلك ادخل وصف الخزنة"
+                      {...field}
+                      className="h-24"
                     />
-                    </div>
-                    <div className="col-span-1">
-                        <FormField
-                        control={form.control}
-                        name="vaultName"
-                        render={({ field }) => (
-                            <FormItem>
-                            <FormLabel>اسم الخزنة</FormLabel>
-                            <FormControl>
-                                <Input placeholder="من فضلك ادخل الكود" {...field} />
-                            </FormControl>
-                            <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    </div>
-                </div>
-                <div className="flex justify-end gap-4">
-                    <Button variant="default" className="mr-2">حفظ</Button>
-                </div>
-            </form>
-        </Form>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="flex justify-end gap-4">
+            <Button variant="default" className="mr-2">
+              إضافة جديدة
+            </Button>
+            <Button variant="default" className="mr-2">
+              الرجوع للرئيسية
+            </Button>
+          </div>
+        </form>
+      </Form>
     </div>
-  )
-}
+  );
+};
 
-export default AccountsTree
+export default AccountsTree;
--- src/pages/AddVault.tsx
+++ src/pages/AddVault.tsx
@@ -14,7 +14,6 @@ import {
   FormMessage,
 } from "@components/ui/form";
 import Heading from "../components/common/Heading/Heading";
-import React from "react";
 
 const AddVault = () => {
   const form = useForm<z.infer<typeof AddVaultSchema>>({
@@ -26,83 +25,86 @@ const AddVault = () => {
   };
   return (
     <div className="page__container">
-    <Heading title="اضافة خزنة"/>
-    <Form {...form}>
-      <form
-        className="pt-8 p-6"
-        onSubmit={form.handleSubmit(submitForm)}
-      >
-        <div className="grid grid-cols-3 gap-4 mb-4">
-          <div className="col-span-1">
-            <FormField
-              control={form.control}
-              name="vaultCode"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>كود الخزنة</FormLabel>
-                  <FormControl>
-                    <Input placeholder="من فضلك ادخل كود الخزنة" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
-          <div className="col-span-1">
-            <FormField
-              control={form.control}
-              name="vaultName"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>اسم الخزنة</FormLabel>
-                  <FormControl>
-                    <Input placeholder="من فضلك ادخل اسم الخزنة" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
-          <div className="col-span-1">
-            <FormField
-              control={form.control}
-              name="currentBalance"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>رصيد الخزنة الحالي</FormLabel>
-                  <FormControl>
-                    <Input placeholder="من فضلك ادخل رصيد الخزنة" {...field} />
-                  </FormControl>
+      <Heading title="اضافة خزنة" />
+      <Form {...form}>
+        <form className="pt-8 p-6" onSubmit={form.handleSubmit(submitForm)}>
+          <div className="grid grid-cols-3 gap-4 mb-4">
+            <div className="col-span-1">
+              <FormField
+                control={form.control}
+                name="vaultCode"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>كود الخزنة</FormLabel>
+                    <FormControl>
+                      <Input placeholder="من فضلك ادخل كود الخزنة" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="col-span-1">
+              <FormField
+                control={form.control}
+                name="vaultName"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>اسم الخزنة</FormLabel>
+                    <FormControl>
+                      <Input placeholder="من فضلك ادخل اسم الخزنة" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="col-span-1">
+              <FormField
+                control={form.control}
+                name="currentBalance"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>رصيد الخزنة الحالي</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="من فضلك ادخل رصيد الخزنة"
+                        {...field}
+                      />
+                    </FormControl>
 
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          </div>
+          <div className="mb-4">
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>وصف الخزنة</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      placeholder="من فضلك ادخل وصف الخزنة"
+                      {...field}
+                    />
+                  </FormControl>
                   <FormMessage />
                 </FormItem>
               )}
             />
           </div>
-        </div>
-        <div className="mb-4">
-          <FormField
-            control={form.control}
-            name="description"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>وصف الخزنة</FormLabel>
-                <FormControl>
-                  <Textarea placeholder="من فضلك ادخل وصف الخزنة" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </div>
-        <div className="flex justify-end gap-4">
-          <Button className="mr-2">الرجوع للرئيسية</Button>
-          <Button variant="secondary" type="submit">
-            إضافة جديدة
-          </Button>
-        </div>
-      </form>
-    </Form>
+          <div className="flex justify-end gap-4">
+            <Button className="mr-2">الرجوع للرئيسية</Button>
+            <Button variant="secondary" type="submit">
+              إضافة جديدة
+            </Button>
+          </div>
+        </form>
+      </Form>
     </div>
   );
 };
