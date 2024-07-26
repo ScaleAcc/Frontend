bee7c8959103f04a323c6c83d8dc1a88d093ff8b
Nabil Alex <nabilmohaed46@gmail.com>
2024-07-26 03:21:47 +0300

    handle login

--- src/validations/login.ts
+++ src/validations/login.ts
@@ -1,6 +1,10 @@
 import { z } from "zod";
 const LoginSchema = z.object({
-  email: z.string().min(2, "من فضلك ادحل ايميل"),
-  password: z.string().min(2, "من فضلك ادحل ايميل").email(),
+  email: z
+    .string({ message: "من فضلك ادخل ايميل " })
+    .min(2, "من فضلك ادحل ايميل"),
+  password: z
+    .string({ message: "من فضلك ادخل كلمة مرور " })
+    .min(2, "من فضلك ادحل كلمة مرور"),
 });
 export { LoginSchema };
